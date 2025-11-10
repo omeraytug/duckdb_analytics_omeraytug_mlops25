02dfb62a1b12b92acbf662b01464667656f2a8b7
Omer Aytug <aytugomer00@gmail.com>
2025-11-10 15:26:52 +0100

    sync

--- code_alongs/03_sql_intro/queries.sql
+++ code_alongs/03_sql_intro/queries.sql
@@ -1,1 +1,7 @@
-SELECT * FROM funny_jokes
+SELECT * FROM funny_jokes;
+
+-- sort by rating, ascending order
+SELECT * FROM  funny_jokes ORDER BY rating;
+
+-- sort by rating, descending order
+SELECT * FROM  funny_jokes ORDER BY rating DESC;
