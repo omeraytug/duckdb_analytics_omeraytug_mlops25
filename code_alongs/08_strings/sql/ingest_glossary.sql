a9380218fdd05106a4fb675157736007f1e9c03c
Ömer Aytug <aytugomer00@gmail.com>
2025-11-19 11:46:49 +0100

    sync

--- code_alongs/08_strings/sql/ingest_glossary.sql
+++ code_alongs/08_strings/sql/ingest_glossary.sql
@@ -1,7 +1,16 @@
 CREATE TABLE
-    IF NOT EXISTS glossary.duckdb AS (
+    IF NOT EXISTS glossary AS (
         SELECT
             *
         FROM
             read_csv_auto ('data/sql_terms.csv')
     );
+
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE
+    IF NOT EXISTS staging.glossary AS
+SELECT
+    *
+FROM
+    glossary;
---
+++ code_alongs/08_strings/sql/string_functions.sql
@@ -0,0 +1,14 @@
+SELECT
+    sql_word,
+    UPPER(TRIM(sql_word)) AS cleaned_word,
+    LOWER(TRIM(sql_word)) AS cleaned_lower,
+    'SQL command: ' || cleaned_lower AS concatenated
+FROM
+    staging.glossary;
+
+SELECT
+    description,
+    REPLACE (TRIM(description), '  ', ' ') AS cleaned_description,
+    REGEXP_REPLACE (TRIM(description), ' +', ' ', 'g') AS regex_cleaned
+FROM
+    staging.glossary;
