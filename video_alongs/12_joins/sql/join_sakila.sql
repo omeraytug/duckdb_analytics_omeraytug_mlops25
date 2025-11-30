bd7551be5df27e253d3eb5cc97f0062426e2e078
Omer Aytug <aytugomer00@gmail.com>
2025-11-30 20:01:26 +0100

    sync

---
+++ video_alongs/12_joins/sql/join_sakila.sql
@@ -0,0 +1,73 @@
+-- LEFT JOIN: which actor has played which film_id?
+SELECT
+    a.actor_id,
+    a.first_name,
+    a.last_name,
+    fa.film_id
+FROM
+    actor a
+    LEFT JOIN film_actor fa ON fa.actor_id = a.actor_id;
+
+-- LEFT JOIN: which actor has played which films?
+SELECT
+    a.first_name,
+    a.last_name,
+    f.title
+FROM
+    actor a
+    LEFT JOIN film_actor fa ON fa.actor_id = a.actor_id
+    LEFT JOIN film f ON f.film_id = fa.film_id;
+
+-- get all films with associated category
+SELECT
+    f.title,
+    c.name as category
+FROM
+    film_category fc
+    INNER JOIN category c ON fc.category_id = c.category_id
+    INNER JOIN film f ON f.film_id = fc.film_id;
+
+SELECT
+    COUNT(*)
+FROM
+    film;
+
+SELECT
+    COUNT(*)
+FROM
+    category;
+
+SELECT
+    COUNT(*)
+FROM
+    film f
+    INNER JOIN category c ON category_id;
+
+-- in which address, city and country does the staff live in
+--address_id, first_name, last_name
+FROM
+    staff;
+
+-- address_id, adress, city_id
+FROM
+    address;
+
+-- city_id, city, country_id
+FROM
+    city;
+
+-- country, country_id
+FROM
+    country;
+
+SELECT
+    s.first_name,
+    s.last_name,
+    a.address,
+    c.city,
+    cty.country
+FROM
+    staff s
+    LEFT JOIN address a ON s.address_id = a.address_id
+    LEFT JOIN city c ON a.city_id = c.city_id
+    LEFT JOIN country cty ON cty.country_id = c.country_id
